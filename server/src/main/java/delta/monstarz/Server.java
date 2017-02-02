0cfbe8cba2662bbb05a70d79df5d093ab5bcf132
alexwilli <alex@einovie.com>
2017-02-02 13:26:20 -0700

    starting to paste in communication code

--- server/src/main/java/delta/monstarz/Server.java
+++ server/src/main/java/delta/monstarz/Server.java
@@ -1,9 +1,44 @@
 package delta.monstarz;
+import java.io.*;
+import java.net.*;
+import com.sun.net.httpserver.*;
 
 public class Server {
 
-	public static void main(String[] args){
-		System.out.println("I'm a server");
+	private static final int MAX_WAITING_CONNECTIONS = 12;
+
+	private HttpServer server;
+
+	private void run(String portNumber) {
+		System.out.println("Initializing HTTP Server");
+		try {
+			server = HttpServer.create(
+					new InetSocketAddress(Integer.parseInt(portNumber)),
+					MAX_WAITING_CONNECTIONS);
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+
+		server.setExecutor(null); // use the default executor
+
+		System.out.println("Creating contexts");
+		// server.createContext("/games/list", new ListGamesHandler());
+		server.createContext("/command", new ServerCommunicator());
+
+		System.out.println("Starting server");
+		server.start();
 	}
 
+	public static void main(String[] args) {
+		System.out.println("I'm a server. Currently I just echo");
+		String portNumber;
+		if(args.length == 0) {
+			portNumber = "8888";
+		} else {
+			portNumber = args[0];
+		}
+		new Server().run(portNumber);
+	}
 }
---
+++ server/src/main/java/delta/monstarz/ServerCommunicator.java
@@ -0,0 +1,73 @@
+package delta.monstarz;
+import java.io.*;
+import java.net.*;
+import com.sun.net.httpserver.*;
+
+/**
+ * Created by oliphaun on 2/2/17.
+ */
+
+public class ServerCommunicator implements HttpHandler {
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+
+        boolean success = false;
+
+        try {
+            if (exchange.getRequestMethod().toLowerCase().equals("post")) {
+
+                Headers reqHeaders = exchange.getRequestHeaders();
+                if (reqHeaders.containsKey("Authorization")) {
+
+                    String authToken = reqHeaders.getFirst("Authorization");
+                    if (authToken.equals("afj232hj2332")) {
+
+                        InputStream reqBody = exchange.getRequestBody();
+                        String reqData = readString(reqBody);
+//                        BaseCommand command = SerDes.deserializeCommand(reqData, "");
+//                        System.out.println(command.getName());
+//                        // System.out.println(reqData);
+//
+//                        IProcessor proc = StringProcessor.getInstance();
+//                        Result res = proc.executeCommand(command);
+//                        String ser = SerDes.serialize(res);
+//
+//                        exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
+
+                        OutputStream respBody = exchange.getResponseBody();
+                        writeString(reqData, respBody); //ser
+                        respBody.close();
+                        success = true;
+                    }
+                }
+            }
+
+            if (!success) {
+                exchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_REQUEST, 0);
+            }
+        }
+        catch (IOException e) {
+            exchange.sendResponseHeaders(HttpURLConnection.HTTP_SERVER_ERROR, 0);
+            e.printStackTrace();
+        }
+    }
+
+    private String readString(InputStream is) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        InputStreamReader sr = new InputStreamReader(is);
+        char[] buf = new char[1024];
+        int len;
+        while ((len = sr.read(buf)) > 0) {
+            sb.append(buf, 0, len);
+        }
+        return sb.toString();
+    }
+
+    private void writeString(String str, OutputStream os) throws IOException {
+        OutputStreamWriter sw = new OutputStreamWriter(os);
+        sw.write(str);
+        sw.flush();
+    }
+}
+
