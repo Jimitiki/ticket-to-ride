53b7808c53bc04f72b819e39492ebacf84040766
Brad Carter <bradcarter1994@gmail.com>
2017-02-04 11:40:31 -0700

    Created GameSelector activity stub and layout.

---
+++ app/src/main/java/deltamonstarz/tickettoride/views/GameSelectorActivity.java
@@ -0,0 +1,18 @@
+package deltamonstarz.tickettoride.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import deltamonstarz.tickettoride.R;
+
+
+public class GameSelectorActivity extends AppCompatActivity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_game_selector);
+	}
+}
