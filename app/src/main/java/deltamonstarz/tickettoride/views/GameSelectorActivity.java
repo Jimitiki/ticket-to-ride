43e9a049f0e2e965485a9b611dd4a630585de4ac
Brad Carter <bradcarter1994@gmail.com>
2017-02-11 09:36:41 -0800

    Implemented Game Selector Activity

--- app/src/main/java/deltamonstarz/tickettoride/views/GameSelectorActivity.java
+++ app/src/main/java/deltamonstarz/tickettoride/views/GameSelectorActivity.java
@@ -4,12 +4,25 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+
+import delta.monstarz.shared.GameInfo;
 import deltamonstarz.tickettoride.R;
 
 
 public class GameSelectorActivity extends AppCompatActivity
 {
+	//Widgets
+	private RecyclerView mRecyclerView;
+	private LinearLayoutManager mLayoutManager;
+	private RecyclerView.Adapter mAdapter;
+
 
 	public static Intent newIntent(Context packageContext)
 	{
@@ -22,5 +35,28 @@ public class GameSelectorActivity extends AppCompatActivity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game_selector);
+
+		mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+		// use this setting to improve performance if you know that changes
+		// in content do not change the layout size of the RecyclerView
+		mRecyclerView.setHasFixedSize(true);
+
+		// use a linear layout manager
+		mLayoutManager = new LinearLayoutManager(this);
+		mRecyclerView.setLayoutManager(mLayoutManager);
+
+		// specify an adapter (see also next example)
+		GameInfo[] array = new GameInfo[]
+		{
+			new GameInfo("Name", "Owner", 0, new Date(), 2, false),
+			new GameInfo("Team Cap", "Steve", 1, new Date(), 3, false),
+			new GameInfo("Bikini Bottom", "Owner", 2, new Date(), 4, true),
+			new GameInfo("Team Iron", "Owner", 3, new Date(), 4, false),
+			new GameInfo("Ooo", "Owner", 4, new Date(), 5, true),
+		};
+		ArrayList<GameInfo> myDataset = new ArrayList<>(Arrays.asList(array));;
+		mAdapter = new GameSelectionRecyclerAdapter(myDataset);
+		mRecyclerView.setAdapter(mAdapter);
 	}
 }
