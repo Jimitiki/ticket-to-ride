48b0904ba7bc0c14661498446a690d539c51d922
Alex Williams <alex@einovie.com>
2017-02-14 17:38:53 -0700

    fixed problem with logging in and registering. Now it only errors out if you put the wrong credentials in

--- server/src/main/java/delta/monstarz/server/web/HandleCommand.java
+++ server/src/main/java/delta/monstarz/server/web/HandleCommand.java
@@ -5,7 +5,6 @@ import com.sun.net.httpserver.HttpExchange;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.util.Map;
 
