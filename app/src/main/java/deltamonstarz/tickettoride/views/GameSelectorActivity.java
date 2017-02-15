c64ac8a63f43bbe9318effbe3662835e32f6bc89
trevorlyman <trevorlyman94@gmail.com>
2017-02-14 19:44:03 -0700

    Fixed the GameSelector activity

--- app/src/main/java/deltamonstarz/tickettoride/views/GameSelectorActivity.java
+++ app/src/main/java/deltamonstarz/tickettoride/views/GameSelectorActivity.java
@@ -6,11 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 
 import delta.monstarz.shared.GameInfo;
@@ -75,4 +76,104 @@ public class GameSelectorActivity extends AppCompatActivity
 		Intent i = GameActivity.newIntent(GameSelectorActivity.this);
 		startActivity(i);
 	}
+
+
+
+
+
+	private class GameHolder extends RecyclerView.ViewHolder {
+		public TextView gameName;
+		public TextView gameOwner;
+		public TextView gameStarted;
+		public TextView playerCount;
+
+		public GameInfo gameInfo;
+
+		public GameHolder(View v) {
+			super(v);
+			gameName = (TextView) v.findViewById(R.id.nameText);
+			gameOwner = (TextView) v.findViewById(R.id.ownerText);
+			gameStarted = (TextView) v.findViewById(R.id.gameStarted);
+			playerCount = (TextView) v.findViewById(R.id.playerCountText);
+
+			v.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					GameSelectorPresenter.getInstance().joinGame(gameInfo.getGameID());
+				}
+			});
+
+		}
+	}
+
+
+
+	private class GameSelectionRecyclerAdapter extends RecyclerView.Adapter<GameHolder>
+	{
+		private List<GameInfo> mGameList;
+
+
+
+		public GameSelectionRecyclerAdapter(List<GameInfo> myGameList) {
+			mGameList = myGameList;
+		}
+
+
+
+		// Create new views (invoked by the layout manager)
+		@Override
+		public GameHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+			// create a new view
+			View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.game_selector_recycler_view, parent, false);
+			// set the view's size, margins, padding and layout parameters
+			GameHolder vh = new GameHolder(v);
+			return vh;
+		}
+
+		// Replace the contents of a view (invoked by the layout manager)
+		@Override
+		public void onBindViewHolder(final GameHolder holder, int position) {
+
+			holder.gameInfo = mGameList.get(position);
+
+			holder.gameName.setText(holder.gameInfo.getName());
+			holder.gameOwner.setText(holder.gameInfo.getOwnerName());
+			holder.playerCount.setText(String.valueOf(holder.gameInfo.getPlayerCount()));
+
+			if (holder.gameInfo.isGameStarted()) {
+				holder.gameStarted.setText("Yes");
+			}
+			else{
+				holder.gameStarted.setText("No");
+			}
+		}
+
+		// Return the size of your dataset (invoked by the layout manager)
+		@Override
+		public int getItemCount() {
+			return mGameList.size();
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
