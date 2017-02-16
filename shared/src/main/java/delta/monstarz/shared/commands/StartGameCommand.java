8e10c8f78bd2f242d46bff5ae32b7f480b08c346
Jimitiki <cwjohnson42@gmail.com>
2017-02-15 17:54:11 -0700

    made start game commands global

--- shared/src/main/java/delta/monstarz/shared/commands/StartGameCommand.java
+++ shared/src/main/java/delta/monstarz/shared/commands/StartGameCommand.java
@@ -1,13 +1,10 @@
 package delta.monstarz.shared.commands;
 
-/**
- * Created by cwjohn42 on 2/15/17.
- */
-
 public class StartGameCommand extends BaseCommand {
 	public StartGameCommand(String username, int gameID) {
 		super(username, gameID);
 		name = "StartGameCommand";
+		isGlobal = true;
 	}
 
 	@Override
