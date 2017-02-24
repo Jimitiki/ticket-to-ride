20cf5947d3665d76a854d7956f092856ddc3347a
Jimitiki <cwjohnson42@gmail.com>
2017-02-24 12:44:33 -0700

    Bound functions to buttons in the game fragment

--- app/src/main/java/deltamonstarz/tickettoride/views/gamePlay/GameFragment.java
+++ app/src/main/java/deltamonstarz/tickettoride/views/gamePlay/GameFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import deltamonstarz.tickettoride.R;
 import deltamonstarz.tickettoride.presenters.GamePresenter;
@@ -16,6 +17,12 @@ import deltamonstarz.tickettoride.presenters.GamePresenter;
  */
 public class GameFragment extends Fragment {
 	private static GamePresenter presenter;
+	private Button drawCard;
+	private Button placeTrain;
+	private Button viewCards;
+	private Button viewHistory;
+	private Button viewChat;
+	private Button demo;
 
 	public GameFragment() {}
 
@@ -40,6 +47,59 @@ public class GameFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	                         Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_game, container, false);
+		drawCard = (Button) v.findViewById(R.id.draw_card_button);
+		placeTrain = (Button) v.findViewById(R.id.place_train_button);
+		viewCards = (Button) v.findViewById(R.id.destination_cards_button);
+		viewHistory = (Button) v.findViewById(R.id.history_button);
+		viewChat = (Button) v.findViewById(R.id.chat_button);
+		demo = (Button) v.findViewById(R.id.demo_button);
+
+		drawCard.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				System.out.print("drawing card");
+			}
+		});
+
+		placeTrain.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				System.out.print("claiming route");
+			}
+		});
+
+		viewCards.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				System.out.print("opening destination card view");
+			}
+		});
+
+		viewHistory.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				System.out.print("opening history view");
+			}
+		});
+
+		viewChat.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				System.out.print("opening chat");
+			}
+		});
+
+		demo.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				advanceDemo();
+			}
+		});
+
 		return v;
+	}
+
+	private void advanceDemo() {
+
 	}
 }
