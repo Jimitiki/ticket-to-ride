151a28758f2603fa28043864c737e9fac05d6159
Brad Carter <bradcarter1994@gmail.com>
2017-02-25 11:05:53 -0700

    Implemented City bean

--- shared/src/main/java/delta/monstarz/shared/City.java
+++ shared/src/main/java/delta/monstarz/shared/City.java
@@ -1,8 +1,41 @@
 package delta.monstarz.shared;
 
+import java.awt.Point;
+
 /**
  * Created by Brad Carter on 2/21/2017.
  */
 
 public class City {
+	//Data Members
+	private String mName;
+	private Point mPosition;
+
+	//Constructor
+	public City(String pName, Point pPosition)
+	{
+		mName = pName;
+		mPosition = pPosition;
+	}
+
+	//Getters and Setters
+	public String getName()
+	{
+		return mName;
+	}
+
+	public void setName(String pName)
+	{
+		mName = pName;
+	}
+
+	public Point getPosition()
+	{
+		return mPosition;
+	}
+
+	public void setPosition(Point pPosition)
+	{
+		mPosition = pPosition;
+	}
 }
