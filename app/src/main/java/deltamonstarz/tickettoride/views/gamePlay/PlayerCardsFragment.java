9242a5cbcf78b9b6145339a24fbcda859433ea94
Trevor Lyman <trevorlyman94@gmail.com>
2017-02-25 11:22:22 -0700

    Fixed UI so that it fits on larger screens. Added train cards and counts.

--- app/src/main/java/deltamonstarz/tickettoride/views/gamePlay/PlayerCardsFragment.java
+++ app/src/main/java/deltamonstarz/tickettoride/views/gamePlay/PlayerCardsFragment.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 
 import java.io.IOException;
@@ -22,7 +23,17 @@ import deltamonstarz.tickettoride.R;
  */
 public class PlayerCardsFragment extends Fragment {
 
-	static final String BASE_PATH = "train_cards/train_cards_";
+	static final String BASE_PATH = "train_cards/train_card_";
+
+	TextView redCount;
+	TextView whiteCount;
+	TextView orangeCount;
+	TextView greenCount;
+	TextView blueCount;
+	TextView blackCount;
+	TextView yellowCount;
+	TextView pinkCount;
+	TextView goldCount;
 
 	ImageView redImg;
 	ImageView whiteImg;
@@ -34,10 +45,6 @@ public class PlayerCardsFragment extends Fragment {
 	ImageView pinkImg;
 	ImageView goldImg;
 
-
-
-
-
 	public PlayerCardsFragment() {
 		// Required empty public constructor
 	}
@@ -47,6 +54,17 @@ public class PlayerCardsFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
 		View view = inflater.inflate(R.layout.fragment_player_cards, container, false);
+
+		redCount = (TextView) view.findViewById(R.id.card_count_01_red);
+		whiteCount = (TextView) view.findViewById(R.id.card_count_02_white);
+		orangeCount = (TextView) view.findViewById(R.id.card_count_03_orange);
+		greenCount = (TextView) view.findViewById(R.id.card_count_04_green);
+		blueCount = (TextView) view.findViewById(R.id.card_count_05_blue);
+		blackCount = (TextView) view.findViewById(R.id.card_count_06_black);
+		yellowCount = (TextView) view.findViewById(R.id.card_count_07_yellow);
+		pinkCount = (TextView) view.findViewById(R.id.card_count_08_pink);
+		goldCount = (TextView) view.findViewById(R.id.card_count_09_gold);
+
 
 		redImg = (ImageView) view.findViewById(R.id.card_image_01_red);
 		whiteImg = (ImageView) view.findViewById(R.id.card_image_02_white);
@@ -58,6 +76,28 @@ public class PlayerCardsFragment extends Fragment {
 		pinkImg = (ImageView) view.findViewById(R.id.card_image_08_pink);
 		goldImg = (ImageView) view.findViewById(R.id.card_image_09_gold);
 
+		setCardCounts();
+		setCardImages();
+
+
+		return view;
+	}
+
+	void setCardCounts(){
+		// Todo: Access the model to set the values
+
+		redCount.setText("10");
+		whiteCount.setText("20");
+		orangeCount.setText("30");
+		greenCount.setText("40");
+		blueCount.setText("50");
+		blackCount.setText("60");
+		yellowCount.setText("70");
+		pinkCount.setText("80");
+		goldCount.setText("90");
+	}
+
+	void setCardImages(){
 		setImage(redImg, BASE_PATH + "red.PNG");
 		setImage(whiteImg, BASE_PATH + "white.PNG");
 		setImage(orangeImg, BASE_PATH + "orange.PNG");
@@ -67,9 +107,6 @@ public class PlayerCardsFragment extends Fragment {
 		setImage(yellowImg, BASE_PATH + "yellow.PNG");
 		setImage(pinkImg, BASE_PATH + "pink.PNG");
 		setImage(goldImg, BASE_PATH + "gold.PNG");
-
-
-		return view;
 	}
 
 	void setImage(ImageView imageView, String filePath){
