d4d64eafe3142b40278de154d096b8b7a9a089cf
Brad Carter <bradcarter1994@gmail.com>
2017-02-25 11:09:37 -0700

    Created exception for improper preferences json format

---
+++ server/src/main/java/delta/monstarz/exceptions/PreferenceFormatException.java
@@ -0,0 +1,12 @@
+package delta.monstarz.exceptions;
+
+/**
+ * @author bradcarter
+ */
+public class PreferenceFormatException extends Exception
+{
+	public PreferenceFormatException(String message)
+	{
+		super(message);
+	}
+}
