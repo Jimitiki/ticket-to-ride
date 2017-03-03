d12b68c773f34bbdb79860cbe5d7145ff122e693
Brad Carter <bradcarter1994@gmail.com>
2017-03-03 08:37:33 -0700

    Implemented Destination Deck

--- server/src/main/java/delta/monstarz/server/DestinationDeck.java
+++ server/src/main/java/delta/monstarz/server/DestinationDeck.java
@@ -1,5 +1,8 @@
 package delta.monstarz.server;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 import delta.monstarz.shared.model.IDestinationDeck;
@@ -10,26 +13,63 @@ import delta.monstarz.shared.model.DestCard;
  */
 public class DestinationDeck implements IDestinationDeck
 {
-	@Override
-	public List<DestCard> drawCards()
-	{
-		return null;
+	//Data Members
+	LinkedList<DestCard> deck;
+
+	//Constructor
+	public DestinationDeck() {
+		deck = new LinkedList<>();
 	}
 
-	@Override
-	public void returnCard(DestCard card)
-	{
-
+	public void addCard(DestCard card) {
+		deck.add(card);
 	}
 
+	/**
+	 * Shuffles the deck of destination cards
+	 */
+	public void shuffle() {
+		Collections.shuffle(deck);
+	}
+
+	/**
+	 * Takes three cards from the destination deck
+	 *
+	 * @return Three destination cards in a list
+	 */
+	@Override
+	public List<DestCard> drawCards() {
+		List<DestCard> cards = new ArrayList<>();
+		for(int i = 0; i < 3; i++) {
+			cards.add(deck.removeFirst());
+		}
+		return cards;
+	}
+
+	/**
+	 * Returns a card to the deck.
+	 */
+	@Override
+	public void returnCard(DestCard card) {
+		deck.addLast(card);
+	}
+
+	/**
+	 * Returns a list of cards to the deck.
+	 */
 	@Override
 	public void returnCards(List<DestCard> cards) {
-
+		for(DestCard card : cards) {
+			returnCard(card);
+		}
 	}
 
+	/**
+	 * Removes all cards from the deck.
+	 */
 	@Override
 	public void clear()
 	{
-
+		deck = new LinkedList<>();
 	}
 }
