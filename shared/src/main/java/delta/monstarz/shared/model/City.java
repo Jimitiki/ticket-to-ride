a80e46170257bd31aab6a4068e8d630805a851c5
Alex Williams <alex@einovie.com>
2017-03-03 12:39:11 -0700

    Revert: added DestCardChoices to Player (me)

---
+++ shared/src/main/java/delta/monstarz/shared/model/City.java
@@ -0,0 +1,18 @@
+package delta.monstarz.shared.model;
+
+/**
+ * Created by oliphaun on 2/24/17.
+ */
+
+public class City {
+    private String name;
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
