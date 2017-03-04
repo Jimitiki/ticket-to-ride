295e77f9c968e559e706ae1fad232b7e1535d004
Brad Carter <bradcarter1994@gmail.com>
2017-03-03 20:50:12 -0700

    Implemented Server Draw Train Command

--- server/src/main/java/delta/monstarz/model/commands/ServerDrawTrainCardCommand.java
+++ server/src/main/java/delta/monstarz/model/commands/ServerDrawTrainCardCommand.java
@@ -1,16 +1,32 @@
 package delta.monstarz.model.commands;
 
+import java.util.List;
+
+import delta.monstarz.model.GameManager;
+import delta.monstarz.model.game.Game;
+import delta.monstarz.model.game.manager.PlayerManager;
 import delta.monstarz.shared.commands.DrawTrainCardCommand;
+import delta.monstarz.shared.model.Player;
+import delta.monstarz.shared.model.TrainCard;
 
 /**
  * @author bradcarter
  */
 public class ServerDrawTrainCardCommand extends DrawTrainCardCommand
 {
+	//Data Members
+	private Player p;
+
 	//Constructors
 	public ServerDrawTrainCardCommand(String username, int gameID, int drawPileID)
 	{
 		super(username, gameID, drawPileID);
+	}
+
+	public ServerDrawTrainCardCommand(Player p, int gameID, int drawPileID)
+	{
+		super(p.getUsername(), gameID, drawPileID);
+		this.p = p;
 	}
 
 	//Object Methods
@@ -21,7 +37,12 @@ public class ServerDrawTrainCardCommand extends DrawTrainCardCommand
 	@Override
 	public void execute()
 	{
-		//TODO Implement this
+		Game game = GameManager.getInstance().getGameByID(gameID);
+		TrainCard card = game.getTrainDeck().drawCard();
+		p.drawTrainCard(card);
+		DrawTrainCardCommand command = new DrawTrainCardCommand(username, gameID, -1);
+		command.setCardDrawn(card);
+		game.getHistory().add(command);
 	}
 
 
