becea762738fa9bc76d84f697bdd128a7fee700f
Brad Carter <bradcarter1994@gmail.com>
2017-03-03 19:31:24 -0700

    Implemented method to convert string to enumeration

--- shared/src/main/java/delta/monstarz/shared/model/CardColor.java
+++ shared/src/main/java/delta/monstarz/shared/model/CardColor.java
@@ -1,5 +1,6 @@
 package delta.monstarz.shared.model;
 
+
 /**
  * @author bradcarter
  */
@@ -14,5 +15,33 @@ public enum CardColor
 	ORANGE,
 	WHITE,
 	GOLD,
-	UNKNOWN
+	UNKNOWN;
+
+	public static CardColor fromString(String s) {
+		switch(s) {
+			case "blue":
+				return BLUE;
+			case "green":
+				return GREEN;
+			case "red":
+				return RED;
+			case "yellow":
+				return YELLOW;
+			case "black":
+				return BLACK;
+			case "orange":
+				return ORANGE;
+			case "white":
+				return WHITE;
+			case "gold":
+				return GOLD;
+
+
+
+			default:
+				return UNKNOWN;
+
+
+		}
+	}
 }
