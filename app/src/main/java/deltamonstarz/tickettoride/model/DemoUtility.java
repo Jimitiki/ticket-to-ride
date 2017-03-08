49c22261d81517b273fabc7a0d2666d49131e4a1
Trevor Lyman <trevorlyman94@gmail.com>
2017-03-08 15:35:08 -0700

    Added Demo1

--- app/src/main/java/deltamonstarz/tickettoride/model/DemoUtility.java
+++ app/src/main/java/deltamonstarz/tickettoride/model/DemoUtility.java
@@ -3,6 +3,8 @@ package deltamonstarz.tickettoride.model;
 import android.content.Context;
 import android.widget.Toast;
 
+import java.util.List;
+
 import delta.monstarz.shared.commands.DrawTrainCardCommand;
 import delta.monstarz.shared.model.CardColor;
 import delta.monstarz.shared.model.PlayerInfo;
@@ -71,8 +73,9 @@ public class DemoUtility {
 
 	private static void demo0(){
 
-		// Draw blue card
+		// Draw gold card
 		ClientDrawTrainCardCommand commandCard = new ClientDrawTrainCardCommand(model.getUsername(), model.getGameID(), 0);
+		commandCard.setCardDrawn(new TrainCard(CardColor.GOLD));
 		commandCard.execute();
 
 		// Update player info
@@ -87,12 +90,29 @@ public class DemoUtility {
 		ClientUpdatePlayerInfoCommand commandInfo = new ClientUpdatePlayerInfoCommand(changedInfo);
 		commandInfo.execute();
 
-		showToast("Drew a new blue train card");	commandCard.setCardDrawn(new TrainCard(CardColor.BLUE));
+		showToast("Drew a new gold train card");	commandCard.setCardDrawn(new TrainCard(CardColor.BLUE));
 
 	}
 
 	private static void demo1(){
+		// Opponent draws two train cards
 
+		List<PlayerInfo> infos = model.getPlayerInfos();
+
+		PlayerInfo playerInfo = null;
+		for (int i = 0; i < infos.size(); i++){
+			if (!model.getUsername().equals(infos.get(i).getUsername())){
+				playerInfo = infos.get(i);
+				i = infos.size();
+			}
+		}
+
+
+		playerInfo.setNumTrainsCards(playerInfo.getNumTrainsCards() + 2);
+		ClientUpdatePlayerInfoCommand command = new ClientUpdatePlayerInfoCommand(playerInfo);
+		command.execute();
+
+		showToast("Opponent drew two train cards");
 	}
 
 	private static void demo2(){
