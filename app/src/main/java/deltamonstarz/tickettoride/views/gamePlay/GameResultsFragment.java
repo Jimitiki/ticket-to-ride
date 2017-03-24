2a649af1370d85a85a3f5b46afd5e944813e4842
Alex Williams <alex@einovie.com>
2017-03-24 13:16:15 -0600

    stuff

--- app/src/main/java/deltamonstarz/tickettoride/views/gamePlay/GameResultsFragment.java
+++ app/src/main/java/deltamonstarz/tickettoride/views/gamePlay/GameResultsFragment.java
@@ -2,17 +2,22 @@ package deltamonstarz.tickettoride.views.gamePlay;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import delta.monstarz.shared.GameInfo;
 import deltamonstarz.tickettoride.R;
 import deltamonstarz.tickettoride.model.ClientModel;
 import deltamonstarz.tickettoride.presenters.GamePresenter;
+import deltamonstarz.tickettoride.presenters.GameSelectorPresenter;
+import deltamonstarz.tickettoride.views.GameSelectorActivity;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -50,23 +55,58 @@ public class GameResultsFragment extends Fragment {
 		return v;
 	}
 
-	public void onPlayerJoin(List<String> players) {
-		StringBuilder sb = new StringBuilder("Players: ");
-		for(int i = 0; i < players.size(); i++)
-		{
-			sb.append(players.get(i));
-			sb.append(i < players.size() - 1 ? ", " : "");
-		}
-		sb.append(".");
-		playerText.setText(sb.toString());
-		if (players.size() > 1 && !ClientModel.getInstance().isStarted()) {
-			startGameButton.setEnabled(true);
-			startGameButton.setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					presenter.startGame();
-				}
-			});
+	private class ResultHolder extends RecyclerView.ViewHolder {
+		private TextView player;
+		private TextView final_score;
+		private TextView routes;
+		private TextView finished_destinations;
+		private TextView unfinished_destinations;
+
+		private View view;
+
+		/**
+		 * Initializes a result holder.
+		 *
+		 * @param v The view that belongs the recycler view.
+		 */
+		ResultHolder(View v) {
+			super(v);
+			view = v;
+
+			player = (TextView) v.findViewById(R.id.player);
+			final_score = (TextView) v.findViewById(R.id.final_score);
+			routes = (TextView) v.findViewById(R.id.routes);
+			finished_destinations = (TextView) v.findViewById(R.id.finished_destinations);
+			unfinished_destinations = (TextView) v.findViewById(R.id.unfinished_destinations);
 		}
 	}
+
+	private class GameResultsRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
+	{
+		private List<GameResult> resultList;
+
+		public GameResultsRecyclerAdapter() {
+			resultList = new ArrayList<>();
+		}
+
+		@Override
+		public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+			// create a new view
+			View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.game_results_player_list_item, parent, false);
+			// set the view's size, margins, padding and layout parameters
+			GameResultsFragment.ResultHolder vh = new GameResultsFragment.ResultHolder(v);
+			return vh;
+		}
+
+		@Override
+		public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+		}
+
+		@Override
+		public int getItemCount() {
+			return 0;
+		}
+	}
+
 }
