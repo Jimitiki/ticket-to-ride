83c8ca2bce3292ffd19b9738682f599b17306f6d
Jimitiki <cwjohnson42@gmail.com>
2017-03-27 12:56:24 -0600

    Adding cities to destination cards

--- server/src/main/java/delta/monstarz/model/game/manager/DestinationCardManager.java
+++ server/src/main/java/delta/monstarz/model/game/manager/DestinationCardManager.java
@@ -8,6 +8,7 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
+import delta.monstarz.shared.model.City;
 import delta.monstarz.shared.model.DestCard;
 
 /**
@@ -19,12 +20,17 @@ public class DestinationCardManager
 	private LinkedList<DestCard> deck;
 
 	//Constructor
-	public DestinationCardManager(JsonArray jsonDestinationCards) {
+	public DestinationCardManager(JsonArray jsonDestinationCards, List<City> cities) {
 		deck = new LinkedList<>();
 		for (int i = 0; i < jsonDestinationCards.size(); i++) {
 			JsonObject destCard = jsonDestinationCards.get(i).getAsJsonObject();
+			JsonArray endpointArray = destCard.get("endpoints").getAsJsonArray();
+			String city1 = endpointArray.get(0).getAsString();
+			String city2 = endpointArray.get(1).getAsString();
 
 			DestCard card = new DestCard(destCard);
+			card.setCity1(new City(city1, 0));
+			card.setCity2(new City(city2, 0));
 			deck.add(card);
 		}
 	}
