1fb0175e1e8f144f348a3bf00602294d26b11f1f
Jimitiki <cwjohnson42@gmail.com>
2017-04-15 12:20:37 -0600

    Fixed an error with command handling on the server

--- server/src/main/java/delta/monstarz/web/handler/HandleCommand.java
+++ server/src/main/java/delta/monstarz/web/handler/HandleCommand.java
@@ -16,33 +16,35 @@ import delta.monstarz.web.QueryParser;
 public class HandleCommand extends ServerHandler {
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
+		try {
 
-		Map<String, String> query = QueryParser.parseQuery(exchange.getRequestURI().getRawQuery());
+			if (!checkAuth_sendHeader(exchange)) {
+				// Send auth bad command
+				AuthBadCommand command = new AuthBadCommand("");
+				response = SerDes.serialize(command);
+			} else if (exchange.getRequestMethod().toLowerCase().equals("get")) { //getting command list for a game
+				Map<String, String> query = QueryParser.parseQuery(exchange.getRequestURI().getRawQuery());
+				CommandListCommand command = new CommandListCommand(query.get("username"));
+				command.setCommands(CommandManager.getCommands(Integer.parseInt(query.get("gameID")), query.get("username"),
+						Integer.parseInt(query.get("curCommand"))));
+				response = SerDes.serialize(command);
+			} else if (exchange.getRequestMethod().toLowerCase().equals("post")) { //sending a command to a game
+				InputStream reqBody = exchange.getRequestBody();
+				String reqData = readString(reqBody);
+				BaseCommand command = SerDes.deserializeCommand(reqData, COMMAND_PREFIX);
+				try {
+					CommandManager.execute(command);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 
-		if (!checkAuth_sendHeader(exchange)) {
-			// Send auth bad command
-			AuthBadCommand command = new AuthBadCommand(query.get("username"));
-			response = SerDes.serialize(command);
-		}
-		else if (exchange.getRequestMethod().toLowerCase().equals("get")) { //getting command list for a game
-
-			CommandListCommand command = new CommandListCommand(query.get("username"));
-			command.setCommands(CommandManager.getCommands(Integer.parseInt(query.get("gameID")), query.get("username"),
-					Integer.parseInt(query.get("curCommand"))));
-			response = SerDes.serialize(command);
-		} else if (exchange.getRequestMethod().toLowerCase().equals("post")) { //sending a command to a game
-			InputStream reqBody = exchange.getRequestBody();
-			String reqData = readString(reqBody);
-			BaseCommand command = SerDes.deserializeCommand(reqData, COMMAND_PREFIX);
-			try {
-				CommandManager.execute(command);
-			} catch (Exception e) {
-				e.printStackTrace();
+				response = "{success: ok}";
 			}
-
-			response = "{success: ok}";
+			sendResponse(exchange);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new IOException();
 		}
-		sendResponse(exchange);
 	}
 
 
